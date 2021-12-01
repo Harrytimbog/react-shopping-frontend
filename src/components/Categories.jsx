5c10f2f5be268c3db9981685e2a101309574f9e1
Timilehin Arigbede <Harriemannie@gmail.com>
2021-12-01 21:48:08 +0100

    Design Shopping category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,20 +1,20 @@
-// import styled from "styled-components"
-// import CategoryItem from "./CategoryItem";
-// import { categories } from "../data";
+import styled from "styled-components"
+import CategoryItem from "./CategoryItem";
+import { categories } from "../data";
 
-// const Container = styled.div`
-//   display: flex;
-//   padding: 20px;
-// `;
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+`;
 
-// const Categories = () => {
-//   return (
-//     <Container>
-//       {categories.map((item) => (
-//         <CategoryItem item={item} key={item.id} />
-//       ))}
-//     </Container>
-//   )
-// }
+const Categories = () => {
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem item={item} key={item.id} />
+      ))}
+    </Container>
+  )
+}
 
-// export default Categories
+export default Categories
